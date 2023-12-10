fb0ace408f1dbe2186237330a7fab78a0c9c9c42
Tayla Rios <145400166+taylasagerios@users.noreply.github.com>
2023-12-09 20:24:00 -0800

    fixed the vite file

--- Develop/client/vite.config.js
+++ Develop/client/vite.config.js
@@ -7,12 +7,5 @@ export default defineConfig({
   server: {
     port: 3000,
     open: true,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3001',
-        secure: false,
-        changeOrigin: true
-      }
-    }
   }
 })
